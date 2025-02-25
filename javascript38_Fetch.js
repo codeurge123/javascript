7b0d3162a7c3992c209a1e9e7626c73e361f0398
codeurge123 <bansalyash316@gmail.com>
2025-02-26 00:58:54 +0530

    Fetch Info.

---
+++ javascript38_Fetch.js
@@ -0,0 +1,14 @@
+// Let's Understand about fetch:
+// The global fetch() method starts the process of fetching a resource from the network,returning a promise which fullfilled once the response is avaiable.
+
+
+// For more refer fetch notes : 
+
+
+// Note : Remember one thing that'fetch' ke andar hum object bhe pass kar skta hai 
+// Now see how to pass objects in fetch :
+// fetch('url',Object)
+
+
+
+
